704c3b20b484abd87cce21efb89664e4064b738c
Prerna Kalwani <kalwaniprerna16@gmail.com>
2023-01-24 19:16:47 +0530

    Add files via upload

---
+++ Arrays/Searching/Q2.cpp
@@ -0,0 +1,14 @@
+/*
+Q2. Find the only repetitive element between 1 to N-1
+
+Given an array of size N filled with numbers from 1 to N-1 in random order. The array has only one repetitive element. The task is to find the repetitive element.
+
+Examples:
+
+Input: a[] = {1, 3, 2, 3, 4}
+Output: 3
+Explanation: The number 3 is the only repeating element.
+
+Input: a[] = {1, 5, 1, 2, 3, 4}
+Output: 1
+*/
