eaedf64a0269611e01243e11c4f00f3d0d440b7c
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-07-12 23:56:36 +0530

    Update and rename 1.cpp to Print 1 To N Without Loop.cpp

---
+++ Strivers Sheet/Module 1- Learn the basics/Learn Basic Recursion/Print 1 To N Without Loop.cpp
@@ -0,0 +1,15 @@
+// Solution
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        //Your code here
+        if(N == 0) return;
+
+        printNos(N - 1);
+        
+        cout << N << " ";
+    }
+};
+
