306006f818aa4eb9d56dd4529accb4fe523f083b
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-07-13 00:00:50 +0530

    Create Print N to 1 without loop.cpp

---
+++ Strivers Sheet/Module 1- Learn the basics/Learn Basic Recursion/Print N to 1 without loop.cpp
@@ -0,0 +1,16 @@
+// Solution
+
+class Solution {
+  public:
+    void printNos(int N) {
+        // code here
+        if(N == 0) return;
+        cout << N << " ";
+        printNos(N - 1);
+        
+        
+    }
+};
+
+// TC : O(N)
+// SC : O(N)
