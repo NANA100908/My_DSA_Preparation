02b229094ff460a26c9f4d0037834e54e3ab8a16
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-12-03 22:21:47 +0530

    Update Bubble Sort.cpp

--- Strivers Sheet/Module 2 - Learn Important Sorting Techniques/Sorting- I/Bubble Sort.cpp
+++ Strivers Sheet/Module 2 - Learn Important Sorting Techniques/Sorting- I/Bubble Sort.cpp
@@ -8,15 +8,19 @@ class Solution {
         // Your code here
         int n = arr.size();
         for(int i = n-1;i>=1;i--){
+            int didSwap =0;
             for(int j = 0;j<=i-1;j++){
                 if(arr[j]> arr[j+1]){
                     int temp = arr[j];
                     arr[j]= arr[j+1];
                     arr[j+1] = temp;
+                    didSwap = 1;
                 }
             }
+            if(didSwap == 0) break;
         }
     }
 };
 
-// Time Complexity: O(N^2)
+// Time Complexity: O(N^2) - Average Complexity or Worst case
+//                  O(N) - Best Case - If array is already sorted and didSwap remains zero
