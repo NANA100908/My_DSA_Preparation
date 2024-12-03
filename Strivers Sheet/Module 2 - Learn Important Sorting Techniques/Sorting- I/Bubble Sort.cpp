83c7174439ace800c0d2918d10e738981d4bff21
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-12-03 22:15:44 +0530

    Create Bubble Sort.cpp

---
+++ Strivers Sheet/Module 2 - Learn Important Sorting Techniques/Sorting- I/Bubble Sort.cpp
@@ -0,0 +1,22 @@
+Link:- https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort
+
+// Solution:
+class Solution {
+  public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(vector<int>& arr) {
+        // Your code here
+        int n = arr.size();
+        for(int i = n-1;i>=1;i--){
+            for(int j = 0;j<=i-1;j++){
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+};
+
+// Time Complexity: O(N^2)
