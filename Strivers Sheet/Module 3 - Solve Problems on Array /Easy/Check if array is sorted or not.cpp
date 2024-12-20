4de8e8f4c70d1a26f2a15ffee58e3a66db1efd73
Prerna Kalwani <kalwaniprerna16@gmail.com>
2024-12-21 01:28:39 +0530

    Update Check if array is sorted or not.cpp

--- Strivers Sheet/Module 3 - Solve Problems on Array /Easy/Check if array is sorted or not.cpp
+++ Strivers Sheet/Module 3 - Solve Problems on Array /Easy/Check if array is sorted or not.cpp
@@ -1,1 +1,25 @@
 Link:-https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+
+//Solution
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int cnt =0;
+
+        // check if array is sorted in ascending order
+        for(int i =1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
+            }
+        }
+        if(nums[n-1]>nums[0])
+            cnt++;
+        
+        return cnt<=1;
+    }
+};
+
+//Time Complexity = O(N)
+//Space Complexity = O(1)
